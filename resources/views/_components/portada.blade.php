aca1ef4a23eb695356a8d27f4b4d51dc55d26e7a
Gabriel Borgonovo <gabrielalejandroborgonovo@gmail.com>
2024-03-12 16:47:18 -0300

    Added HTML structure and initial styles for the home page slider.

---
+++ resources/views/_components/portada.blade.php
@@ -0,0 +1,35 @@
+<div class="slider">
+    <div class="slider-slides">
+        <div class="slider-slide active d-flex justify-content-between">
+            <div class="align-self-center w-50 d-flex flex-column align-items-start">
+                <h4 class="display-3 text-start"><b class="text-warning">Simona</b> Ecommerce</h4>
+                <h5 class="text-start">Tienda de indumentaria y moda</h5>  
+                <p class="text-start">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur omnis voluptates optio cupiditate iste at iusto, aliquid earum, commodi qui ea nisi explicabo debitis reiciendis sunt libero velit sint consequuntur.</p>
+                <button class="btn btn-warning">Ver productos</button>
+            </div>
+            
+            <img src="/imgs/slider1.jpg" alt="1">
+        </div>
+        <div class="slider-slide">
+            <h4>Diapositiva 2</h4>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque neque, minima cumque magni quo magnam accusantium, dignissimos consequuntur tempore optio laborum molestiae iste! Pariatur possimus, odit asperiores tempora ipsa ratione.</p>
+        </div>
+        <div class="slider-slide">
+            <h4>slider3</h4>
+        </div>
+        <div class="slider-slide">
+            <h4>Diapositiva 4</h4>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque neque, minima cumque magni quo magnam accusantium, dignissimos consequuntur tempore optio laborum molestiae iste! Pariatur possimus, odit asperiores tempora ipsa ratione.</p>
+        </div>
+    </div>
+
+    {{-- botones --}}
+    <div class="slider-btns">
+        <a href="#" class="prev">
+            <img src="/imgs/chevron-left.svg" alt="left">
+        </a>
+        <a href="#" class="next">
+            <img src="/imgs/chevron-rigth.svg" alt="rigth">
+        </a>
+    </div>
+</div>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,6 +4,7 @@
 
 @section('content')
 
+    @include('_components.portada')
     <h1 class="text-center">Página de Inicio</h1>
 
 @endsection
