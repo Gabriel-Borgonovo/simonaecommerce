09f5297ade62a042de665c85984d87e6a6e32d34
Gabriel Borgonovo <gabrielalejandroborgonovo@gmail.com>
2024-03-16 15:35:43 -0300

    Started the HTML layout for the products and filters on the main page.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,6 +5,24 @@
 @section('content')
 
     @include('_components.portada')
-    <h1 class="text-center">Página de Inicio</h1>
+
+
+    <div class="container min-vh-100 mt-5">
+        <h1 class="text-center">Página de Inicio</h1>
+        <section class="row">
+            <article class="col-12 col-lg-3 bg-info">
+                <h2 class="text-center">Filtros</h2>
+                <div class="ps-5">
+                    <h3>Por prendas</h3>
+                    <h3>Por marcas</h3>
+                </div>
+            </article>
+
+            <article class="col-12 col-lg-9 bg-warning">
+                <h2 class="text-center">Productos</h2>
+            </article>
+        </section>
+    </div>
+    
 
 @endsection
