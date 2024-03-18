8d321de83a35327bc6a2f393790035c055c25efc
Gabriel Borgonovo <gabrielalejandroborgonovo@gmail.com>
2024-03-18 16:48:28 -0300

    styles were added to product cards on homepage for better presentation

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -10,7 +10,7 @@ class ProductController extends Controller
 {
     public function index(): View
     {
-        $products = Product::all();
+        $products = Product::all()->take(10);
         return view('index', compact('products'));
     }
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -20,24 +20,39 @@
                 </div> 
             </article>
 
-            <article class="col-12 col-lg-9 my-5">
-               <div class="bg-ligth border rounded shadow">
+            
+            <div class="col-12 col-lg-9 my-5">
+               <div class="bg-ligth border rounded shadow row">
                     <h2 class="text-center">Productos</h2>
                     @forelse ($products as $product)
-                        <div>
+                        <article class="col-6 col-lg-3 d-flex flex-column card-index mb-3">
+
                             @php
                                 // Decode the detail_imgs JSON string into an array
                                 $detailImgs = json_decode($product->detail_imgs, true);
                             @endphp
-                            <img src="{{ isset($detailImgs[0]) ? $detailImgs[0] : 'default-image.jpg' }}" alt="imagen" class="img-card-index img-fluid">
-                            <small>{{ $product->name }}</small>
-                        </div>
+
+                            <a href="#" class="text-reset text-decoration-none">
+                                <div class="position-relative">
+                                    <img src="{{ isset($detailImgs[0]) ? $detailImgs[0] : 'default-image.jpg' }}" alt="imagen" class="img-card-index img-fluid"/>
+                                    <div class="triangle-offer bg-danger text-light">-30% OFF</div>
+                                </div>
+                            
+                                <div class="d-flex flex-column">
+                                    <h5>{{ $product->name }}</h5>
+                                    <p class="fs-small">{{ $product->short_detail }}</p>
+                                    <small>$ <b class="text-secondary text-decoration-line-through">{{ $product->old_price }}</b> ARS</small>
+                                    <small class="fs-5">$ <b class="text-warning-emphasis">{{ $product->current_price }}</b> ARS</small>
+                                    <p class="mt-2 btn btn-warning fw-regular text-secondary-emphasis">Agregar al carrito</p>
+                                </div>
+                            </a>
+                        </article>
                     @empty
                     <p class="ps-5">No hay productos cargados en el sistema</p> 
                     @endforelse
                 </div> 
-            </article>
-            
+            </div>
+        
         </section>
     </div>
     
